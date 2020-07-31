663878e24d707ebd0fab485ae10fe5c6e87305c7
Andyreyesgo <39089776+Andyreyesgo@users.noreply.github.com>
2020-07-31 02:31:55 -0400

    lasta commit

--- n2/pruebaDB.sql
+++ n2/pruebaDB.sql
@@ -4,44 +4,42 @@ Creación base de datos
 CREATE DATABASE prueba_db
 
 CREATE TABLE categoria(
-prueba_db(# id_categoria SERIAL PRIMARY KEY,
-prueba_db(# nombre_categoria VARCHAR (40),
-prueba_db(# descripcion VARCHAR (50)
-prueba_db(# );
-CREATE TABLE
+id_categoria SERIAL PRIMARY KEY,
+nombre_categoria VARCHAR (40),
+descripcion VARCHAR (50)
+);
 
 
 CREATE TABLE precio_producto(
-prueba_db(# id_precio SERIAL PRIMARY KEY,
-prueba_db(# precio_unitario VARCHAR (10),
-prueba_db(# iva VARCHAR (10),
-prueba_db(# subtotal_producto VARCHAR (20)
-prueba_db(# );
-CREATE TABLE
-prueba_db=# 
+id_precio SERIAL PRIMARY KEY,
+precio_unitario VARCHAR (10),
+iva VARCHAR (10),
+subtotal_producto VARCHAR (20)
+);
 
 
 CREATE TABLE producto(                                              
 id_producto SERIAL PRIMARY KEY ,                                                nombre_producto VARCHAR (40),                                                   descripcion VARCHAR (40),                                                       categoria_id INT REFERENCES categoria(id_categoria),                            precio_id INT REFERENCES precio_producto (id_precio)                            );
-CREATE TABLE
+
 
 
 CREATE TABLE cliente(
-prueba_db(# id_cliente SERIAL PRIMARY KEY,
-prueba_db(# nombre_cliente VARCHAR(50),
-prueba_db(# rut_cliente VARCHAR (40),
-prueba_db(# direccion_cliente VARCHAR (50)
-prueba_db(# );
-CREATE TABLE
+id_cliente SERIAL PRIMARY KEY,
+nombre_cliente VARCHAR(50),
+rut_cliente VARCHAR (40),
+direccion_cliente VARCHAR (50)
+);
 
 CREATE TABLE factura (
-prueba_db(# id_factura SERIAL PRIMARY KEY ,
-prueba_db(# cliente_id INT REFERENCES cliente(id_cliente),
-prueba_db(# producto_id INT REFERENCES producto(id_producto),
-prueba_db(# fecha_factura DATE,
-prueba_db(# unidades_compra VARCHAR (30)
-prueba_db(# );
-CREATE TABLE
+id_factura SERIAL PRIMARY KEY ,
+cliente_id INT REFERENCES cliente(id_cliente),
+producto_id INT REFERENCES producto(id_producto),
+fecha_factura DATE,
+unidades_compra VARCHAR (30)
+);
+
+
+
 
 
 Ingreso de datos a tablas 
@@ -58,6 +56,7 @@ COPY 8
 
 prueba_db=# \copy producto(nombre_producto,descripcion,categoria_id,precio_id) FROM 'Desktop/productos.csv' csv HEADER;
 COPY 8
+
 
 Ingreso datos a clientes:
 
@@ -83,3 +82,36 @@ INSERT 0 1
 prueba_db=# INSERT INTO factura(cliente_id,producto_id,fecha_factura,unidades_compra) VALUES ('12', '6','2020-07-24','1');
 INSERT 0 1
 prueba_db=# 
+
+
+
+
+
+Preguntas
+
+
+
+Select cliente.nombre_cliente , factura.unidades_compra, producto.nombre_producto,precio_producto.subtotal_producto
+From cliente
+Inner Join factura On cliente.id_cliente = factura.cliente_id
+Inner Join producto On factura.producto_id = producto.id_producto
+Inner Join precio_producto On precio_producto.id_precio=producto.precio_id
+
+
+SELECT cliente.nombre_cliente , factura.unidades_compra, producto.nombre_producto,precio_producto.subtotal_producto
+From cliente
+Inner Join factura On cliente.id_cliente = factura.cliente_id
+Inner Join producto On factura.producto_id = producto.id_producto
+Inner Join precio_producto On precio_producto.id_precio=producto.precio_id
+WHERE subtotal_producto >100;
+
+
+
+
+
+SELECT cliente.nombre_cliente , factura.unidades_compra, producto.nombre_producto,precio_producto.subtotal_producto
+From cliente
+Inner Join factura On cliente.id_cliente = factura.cliente_id
+Inner Join producto On factura.producto_id = producto.id_producto
+Inner Join precio_producto On precio_producto.id_precio=producto.precio_id
+WHERE id_producto=‘10’;
